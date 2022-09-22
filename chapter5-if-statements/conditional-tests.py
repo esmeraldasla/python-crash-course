2eb4a23954ba185b65f89977db2df3804596a083
esmeraldalima <mel_limaa6@hotmail.com>
2022-09-22 13:54:42 -0300

    Conditionals Tests written and finished.

--- chapter5-if-statements/conditional-tests.py
+++ chapter5-if-statements/conditional-tests.py
@@ -1,1 +1,25 @@
-"" 
+#Conditional tests
+
+food = 'rice'
+print("Is food =='rice'? I predict True.")
+print(food =='rice')
+print("\nIs food == 'Falafel'? I predict False'")
+print(food == 'falafel')
+
+ice_cream = 'chocolate'
+print("\nIs the ice-cream flavor == 'chocolate'? I think so.")
+print(ice_cream =='chocolate')
+print("\nIs ice_cream == 'strawberry'? I don't think so.")
+print(ice_cream == 'strawberry')
+
+laptop = 'samsung'
+print("\nIs your laptop Samsung brand?")
+print(laptop == 'samsung')
+print("Is your laptop Lenovo brand?")
+print(laptop == 'lenovo')
+
+fruit = 'orange'
+print("\nIs her favorite fruit 'orange'?")
+print(fruit == 'orange')
+print("Is her favorite fruit 'apple'?")
+print(fruit == 'apple')
