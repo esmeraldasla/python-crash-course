88824e8c8fc549972a7244b0b97dcc992953143a
esmeraldalima <mel_limaa6@hotmail.com>
2022-10-05 13:21:38 -0300

    Alien Colors 2 added and written.

---
+++ chapter5-if-statements/alien-colors2.py
@@ -0,0 +1,15 @@
+alien_color1 = 'green'
+
+if alien_color1 == 'green':
+    print("You just earned 5 points")
+
+else:
+    print ("You just earned 10 points")
+
+alien_color1 = 'red'
+
+if alien_color1 == 'green':
+    print("\nYou just earned 5 points")
+
+else:
+    print("\nYou just earned 10 points")
