80a0d7a9ddb2004a63147ceadea5412a15246417
esmeraldalima <mel_limaa6@hotmail.com>
2022-10-05 13:40:50 -0300

    Alien Colors 3 added and written.

---
+++ chapter5-if-statements/alien-colors3.py
@@ -0,0 +1,34 @@
+alien_colors = 'green'
+
+if alien_colors == 'green':
+    print("You just earned 5 points")
+
+elif alien_colors == 'yellow':
+    print("You just earned 10 points")
+
+else:
+    print("You just earned 15 points")
+
+
+alien_colors = 'yellow'
+
+if alien_colors == 'green':
+    print("You just earned 5 points")
+
+elif alien_colors == 'yellow':
+    print("You just earned 10 points")
+
+else:
+    print("You just earned 15 points")
+
+
+alien_colors = 'red'
+
+if alien_colors == 'green':
+    print("You just earned 5 points")
+
+elif alien_colors == 'yellow':
+    print("You just earned 10 points")
+
+else:
+    print("You just earned 15 points")
