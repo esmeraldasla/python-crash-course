3a60b9bc57ebce4d66da233a530617309fe323e7
esmeraldalima <mel_limaa6@hotmail.com>
2023-03-19 16:12:02 -0300

    Messages, send_messages and archived_messages added.

---
+++ functions/archived_messages.py
@@ -0,0 +1,27 @@
+"""The purpose of this program is to show that the original list has retained its messages."""
+
+def show_messages(messages):
+    "Print all the messages in the list"
+    print("Showing all messages")
+    for message in messages:
+        print(message)
+
+
+def send_messages(messages, sent_messages):
+    print("\n Sending all messages:")
+    while messages:
+        current_message = messages.pop()
+        print(current_message)
+        sent_messages.append(current_message)
+
+
+messages = ['Hello there!', 'How are you?', 'Bye bye!']
+show_messages(messages)
+
+sent_messages = []
+send_messages(messages[:], sent_messages)
+
+print("\nFinal lists:")
+print(messages)
+print(sent_messages)
+
---
+++ functions/messages.py
@@ -0,0 +1,7 @@
+def show_messages(messages):
+    "Print all the messages in the list"
+    for message in messages:
+        print(message)
+
+messages = ['Hello there!', 'How are you?', 'Bye bye!']
+show_messages(messages)
